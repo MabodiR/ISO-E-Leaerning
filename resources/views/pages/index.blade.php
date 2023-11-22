c548f1fc0f61c995ce4a86e2e888818328150c59
Mabodi Rofhiwa <rofhiwa.mabodi@gmail.com>
2023-11-22 02:07:44 +0200

    Added index page

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,32 @@
+{{-- Include Header Component --}}
+    <x-header />
+
+   	<!-- Start of slider section
+		============================================= -->
+		<section id="slide" class="slider-section">
+			<div id="slider-item" class="slider-item-details">
+				<div  class="slider-area slider-bg-1 relative-position">
+					<div class="slider-text">
+						<div class="section-title mb20 headline text-center ">
+							<div class="layer-1-3">
+								<h2><span>Empowering Excellence, Ensuring Compliance</span></h2>
+							</div>
+						</div>
+						<div class="layer-1-4">
+							<div id="course-btn">
+								<div class="genius-btn  text-center text-uppercase ul-li-block bold-font">
+									<a href="/courses">Our Courses <i class="fas fa-caret-right"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</section>
+	<!-- End of slider section
+		============================================= -->
+
+
+    {{-- Include Footer Component --}}
+    <x-footer />
