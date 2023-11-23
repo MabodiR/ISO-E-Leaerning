3feca8786100a3f0cf6468b913e787ce144ca1cf
Mabodi Rofhiwa <rofhiwa.mabodi@gmail.com>
2023-11-23 21:43:21 +0200

    Added Js to handle login and Registration

---
+++ public/assets/js/auth/login.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    // Set up CSRF token for all AJAX requests
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    $("#loginUser").on("click", function() {
+        var formData = $("#form-login").serialize();
+        var form = $('#form-login')[0];
+        var secureURL = "/login";
+        var loginButton = $(this);
+
+        loginButton.prop("disabled", true); // Disable the button
+        loginButton.html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...');
+
+        $.ajax({
+            url: secureURL,
+            method: "POST",
+            data: formData,
+            success: function(response) {
+                $('#logModal').modal('hide');
+                form.reset();
+                form.classList.remove('was-validated');
+                $('.invalid-feedback').empty();
+
+                // Once the response is received, revert the button to its original state
+                loginButton.prop("disabled", false); // Enable the button
+                loginButton.text("Login");
+                
+                location.reload();
+            },
+            error: function(xhr, status, error) {
+                // Handle validation errors
+                if (xhr.status === 422) {
+                    var errors = xhr.responseJSON.errors;
+                    // Clear previous error messages
+                    $('.invalid-feedback').empty();
+                    // Display error messages for each field
+                    $.each(errors, function (field, messages) {
+                        var inputField = $('#l_' + field);
+                        inputField.addClass('is-invalid');
+                        var errorContainer = inputField.next('.invalid-feedback');
+                        errorContainer.html(messages.join('<br>'));
+                    });
+                    // Once the response is received, revert the button to its original state
+                    loginButton.prop("disabled", false); // Enable the button
+                    loginButton.text("Login");
+                }
+            }
+        });
+    });
+});
