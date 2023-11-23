ad5db0ce023011c91edb16622a621aba8c3b7078
Mabodi Rofhiwa <rofhiwa.mabodi@gmail.com>
2023-11-23 21:32:03 +0200

    Added api and web routes for the application

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,37 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Categories API
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+// Courses API
+Route::get('/courses', [CourseController::class, 'index']);
+Route::get('/courses/{id}', [CourseController::class, 'show']);
+Route::post('/courses', [CourseController::class, 'store']);
+Route::put('/courses/{id}', [CourseController::class, 'update']);
+Route::delete('/courses/{id}', [CourseController::class, 'destroy']);
+
+// Users API
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+// Route::middleware(['auth:sanctum'])->group(function () {
+    // API routes requiring authentication
+    Route::put('/courses/{id}', [CourseController::class, 'update']);
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::put('/courses/{id}', [CourseController::class, 'update']);
+    Route::delete('/courses/{id}', [CourseController::class, 'destroy']);
+
+    Route::post('/courses', [CourseController::class, 'store']);
+
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
+// });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\RatingController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +16,62 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Main page
+Route::get('/', [CourseController::class, 'main']);
+Route::get('/home', [CourseController::class, 'main']);
 
+// get categories
+Route::get('/getcourses/{category}', [CourseController::class, 'getbycategory']);
+
+
+
+//Auth route
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// course index route
+Route::get('/courses', function () {
+    return view('courses.index');
+});
+
+// view create route
+Route::get('/course/create', function () {
+    return view('courses.create');
+});
+
+//view my courses
+Route::get('/my-courses', function () {
+    return view('courses.mycourses');
+});
+
+// get single course route
+Route::get('/course/{id}', [CourseController::class, 'getcourse'])->name('courses.show');;
+//store courses to db
+Route::post('/courses', [CourseController::class, 'store']);
+
+//get my courses
+Route::get('/mycourses', [CourseController::class, 'mycourses']);
+
+//delete my courses
+Route::delete('/delete-course/{id}', [CourseController::class, 'destroy']);
+
+//edit my course
+Route::post('/update/{id}', [CourseController::class, 'update']);
+Route::get('/course/edit/{id}', [CourseController::class, 'edit']);
+
+//store ratings to db
+Route::post('/ratings', [RatingController::class, 'store']);
+
+
+//store comments to db
+Route::post('/comments', [CommentController::class, 'store']);
+
+// Other pages routes
+Route::get('/{page}', function ($page) {
+    $viewPath = "pages.$page";
+
+    if (view()->exists($viewPath)) {
+        return view($viewPath);
+    } else {
+        return view('404');
+    }
+})->where('page', '.*');
